e4bc7c2507a4b9f1d25e27f4667a89845c53ee80
cornellshakh <kornelshakh@proton.me>
2025-07-16 13:24:59 +0200

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
 import { registerSW } from './lib/serviceWorker.ts';
@@ -7,6 +8,7 @@ import { registerSW } from './lib/serviceWorker.ts';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
 
